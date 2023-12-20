1134a8a984d7ef66863825e82720bee529323988
Gabriel Tonet <gabrieltonet98@gmail.com>
2023-12-19 22:39:48 -0300

    Criando sessão welcome

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,10 @@
 import Header from "../../components/header";
+import Welcome from "../../components/welcome";
 import styles from "./page.module.scss";
 
 export default function Home() {
   return <div className={styles.container}>
     <Header />
+    <Welcome />
   </div>;
 }
