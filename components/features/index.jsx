f9c9d30fcc1906823736996d33f0842cf52215e9
Gabriel Tonet <gabrieltonet98@gmail.com>
2024-01-24 17:11:35 -0300

    Criação do form

--- components/features/index.jsx
+++ components/features/index.jsx
@@ -16,17 +16,17 @@ const Features = () => {
     </div>
     <div className={Styles.feature}>
         <Image src={Icon} alt="Icon" className={Styles.icon} />
-        <h1>Digital Strategy</h1>
+        <h1>Estratégia digital</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
     </div>
     <div className={Styles.feature}>
         <Image src={Icon} alt="Icon" className={Styles.icon} />
-        <h1>Digital Strategy</h1>
+        <h1>Social Midia</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
     </div>
     <div className={Styles.feature}>
         <Image src={Icon} alt="Icon" className={Styles.icon} />
-        <h1>Digital Strategy</h1>
+        <h1>Social Midia</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
     </div>
    </div>
